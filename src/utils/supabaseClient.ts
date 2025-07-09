e27ac7b2134af95e56dfd007d049f4869f7f28f1
Munro Mapper <hello@munromapper.com>
2025-07-10 00:28:49 +0100

    Fixing Deployment Errors

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 export function getSupabaseClient() {
+  console.trace('Creating Supabase client')
   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
   const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
