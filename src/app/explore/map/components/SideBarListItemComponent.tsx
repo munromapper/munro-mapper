f14fb429bf150d4b4ac00080aeef43f0993546b0
Munro Mapper <hello@munromapper.com>
2025-07-17 22:15:24 +0100

    Fixing some errors

--- src/app/explore/map/components/SideBarListItemComponent.tsx
+++ src/app/explore/map/components/SideBarListItemComponent.tsx
@@ -14,7 +14,7 @@ export default function SideBarListItemComponent({ munro }: Props) {
             <div className="p-9 rounded-b-xl border border-dashed border-sage">
                 <div className="mb-6">
                     <h2>{munro.name}</h2>
-                    <h3 className="text-moss mt-2">"{munro.nameMeaning || 'Name Meaning'}"</h3>
+                    <h3 className="text-moss mt-2">&quot;{munro.nameMeaning || 'Name Meaning'}&quot;</h3>
                 </div>
                 <div>
                     <p className="text-l">{munro.description || 'The third highest mountain in Britain, Braeriach is perhaps the finest of the Cairngorms. Reaching its vast summit plateau requires a long approach walk, as this area remains a truly wild place .'}</p>
