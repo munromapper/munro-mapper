5308970ca664299d69f209f396507435b2633bb4
Munro Mapper <hello@munromapper.com>
2025-07-29 21:00:04 +0100

    Adding Final friends function

--- src/app/explore/components/SidebarFriendsButton.tsx
+++ src/app/explore/components/SidebarFriendsButton.tsx
@@ -21,7 +21,6 @@ export default function SidebarFriendsButton({
 
     return (
         <IconLink 
-            transitionWrapper=""
             icon={
                 <div>
                     <FriendsIcon />
