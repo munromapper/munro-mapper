96b6e1a7fec07c8ca222ef20d1a68c6241033d02
Munro Mapper <hello@munromapper.com>
2025-09-02 10:18:27 +0100

    Adding list layout

--- src/app/explore/dashboard/page.tsx
+++ src/app/explore/dashboard/page.tsx
@@ -1,3 +1,10 @@
 // src/app/explore/dashboard/page.tsx
 // This is the dashboard page for exploring munro bagging statistics
 
+export default function DashboardLayout() {
+    return (
+        <div>
+            Dashboard Page
+        </div>
+    )
+}
