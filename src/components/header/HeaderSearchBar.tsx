80aeb4225ffb769c11f386aba1a5172bc2b5ee14
Munro Mapper <hello@munromapper.com>
2025-11-27 14:43:30 +0000

    Fixing search bar bugs

--- src/components/header/HeaderSearchBar.tsx
+++ src/components/header/HeaderSearchBar.tsx
@@ -96,11 +96,10 @@ export default function HeaderSearchBar() {
     userProfile?.preferences?.elevationUnit === "feet" ? "feet" : "metres";
 
   return (
-    <div className="flex-1 relative mx-15">
-        <div className="absolute z-60 w-3.5 h-3.5 top-0 bottom-0 my-auto left-6 text-moss">
+    <div className="flex-1 relative px-4 mx-15">
+        <div className="absolute z-60 w-3.5 h-3.5 top-0 bottom-0 my-auto left-8 text-moss">
           <SearchIcon />
         </div>
-        <div className="absolute z-40 bg-mist w-full h-10 -top-10"></div>
         <input
           ref={inputRef}
           type="text"
@@ -119,17 +118,18 @@ export default function HeaderSearchBar() {
           autoComplete="off"
         />
         {showDropdown && (
-        <ul className="absolute left-[-1%] -top-10 pt-[6.5rem] p-4 w-[102%] no-scrollbar bg-mist text-slate rounded-xl z-30 max-h-80 overflow-y-auto shadow-standard">
+        <ul className="absolute left-0 -top-9 pt-0 p-4 w-full no-scrollbar bg-mist text-slate rounded-xl z-30 max-h-80 overflow-y-auto shadow-standard">
+            <div className="sticky z-20 bg-mist w-full h-12.5 mb-12 left-0 top-0"></div>
             {suggestions.map((munro, idx) => (
             <li
               key={munro.id}
-              className={`px-4 py-3 cursor-pointer rounded-xl hover:bg-pebble transition duration-250 ease-in-out ${
+              className={` relative z-10 px-4 py-3 cursor-pointer rounded-xl hover:bg-pebble transition duration-250 ease-in-out ${
                 activeIndex === idx ? "bg-pebble" : ""
               }`}
               onMouseDown={() => handleSelect(munro)}
               onMouseEnter={() => setActiveIndex(idx)}
             >
-              <div className="flex items-center gap-3">
+              <div className="flex items-center gap-5.5">
                 <span className="w-3.5 h-3.5 text-moss">
                   <SearchIcon />
                 </span>
@@ -147,7 +147,7 @@ export default function HeaderSearchBar() {
             </li>
             ))}
             {suggestions.length === 0 && (
-            <li className="px-4 py-2 text-moss">No results found</li>
+            <li className="relative z-10 px-4 py-2 text-moss">No results found</li>
             )}
         </ul>
         )}
